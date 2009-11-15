26f22778b7fb5043e42cfd77a1ad04269d6691ec
Victor Liu <victor.liu@gmail.com>
2009-11-14 17:06:23 -0800

    Added utility overload to output complex numbers in Mathematica style.

---
+++ NumericTypes/complex_io.hpp
@@ -0,0 +1,27 @@
+#ifndef _COMPLEX_IO_HPP_
+#define _COMPLEX_IO_HPP_
+
+#include <iostream>
+#include <complex>
+#include <limits>
+
+template <class T>
+std::ostream&
+	operator<<(
+		std::ostream& ostr,
+		const std::complex<T>& z
+	)
+{
+	if(std::abs(z.real()*std::numeric_limits<T>::epsilon()) > std::abs(z.imag())){
+		ostr << z.real();
+	}else{
+		if(z.imag() < 0){
+			ostr << z.real() << "-I*" << (-z.imag());
+		}else{
+			ostr << z.real() << "+I*" << z.imag();
+		}
+	}
+	return ostr;
+}
+
+#endif // _COMPLEX_IO_HPP_
