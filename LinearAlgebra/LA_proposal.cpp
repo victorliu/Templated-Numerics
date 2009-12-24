f57ac76920a48f7081ce80d82cb4c55de776ec37
Victor Liu <victor.liu@gmail.com>
2009-12-24 15:00:04 -0800

    Added todo list and proposed reworking of LinearAlgebra template system.

---
+++ LinearAlgebra/LA_proposal.cpp
@@ -0,0 +1,204 @@
+#include <iostream>
+
+template <typename T>
+class MatrixBase{
+public:
+	typedef T value_type;
+};
+
+template <typename T>
+class WritableMatrix : public MatrixBase<T>{
+public:
+	typedef T value_type;
+	virtual const value_type& operator()() const = 0;
+	virtual value_type& operator()() = 0;
+};
+
+template <typename T>
+class NonwritableMatrix : public MatrixBase<T>{
+public:
+	typedef T value_type;
+	virtual value_type operator()() const = 0;
+};
+
+
+
+
+template <class T, class R>
+struct IsWritableMatrix{
+};
+template <class T, class R>
+struct IsWritableMatrix<WritableMatrix<T>, R>{
+	typedef R type;
+};
+
+
+template <class Parent>
+class TrivialView : public WritableMatrix<typename Parent::value_type>{
+	const Parent& parent;
+public:
+	typedef typename Parent::value_type value_type;
+	typedef WritableMatrix<value_type> writable_matrix;
+	
+	TrivialView(const Parent &parent):parent(parent){}
+	const value_type& operator()() const{ return parent(); }
+	value_type& operator()(){
+		Parent &p = const_cast<Parent&>(parent);
+		return p();
+	}
+};
+
+
+
+template <class T>
+class TMatrix : public WritableMatrix<T>{
+	T m;
+public:
+	typedef T value_type;
+	typedef WritableMatrix<T> writable_matrix;
+	
+	TMatrix():m(0){}
+	const value_type& operator()() const{ return m; }
+	value_type& operator()(){ return m; }
+	
+	operator TrivialView<TMatrix<T> >(){
+		return TrivialView<TMatrix<T> >(*this);
+	}
+};
+
+
+
+
+
+template <bool B, class TrueType, class FalseType>
+struct TerneryTruth{
+};
+template <class TrueType, class FalseType>
+struct TerneryTruth<true, TrueType, FalseType>{
+	typedef TrueType type;
+};
+template <class TrueType, class FalseType>
+struct TerneryTruth<false, TrueType, FalseType>{
+	typedef FalseType type;
+};
+
+
+
+template <class Parent, bool RO>
+class TransposeView : public TerneryTruth<RO, NonwritableMatrix<typename Parent::value_type>, WritableMatrix<typename Parent::value_type> >::type{
+	typename TerneryTruth<RO, const Parent, Parent>::type *parent;
+public:
+	typedef typename Parent::value_type value_type;
+	typedef typename TerneryTruth< RO, NonwritableMatrix<typename Parent::value_type>, WritableMatrix<typename Parent::value_type> >::type writable_matrix;
+	typedef typename TerneryTruth<!RO, NonwritableMatrix<typename Parent::value_type>, WritableMatrix<typename Parent::value_type> >::type nonwritable_matrix;
+	
+	TransposeView(const Parent &parent):parent(const_cast<typename TerneryTruth<RO, const Parent, Parent>::type *>(&parent)){}
+	
+	value_type& operator()(){ return (*parent)(); }
+	
+	typename TerneryTruth<RO,value_type,const value_type&>::type operator()() const{ return (*parent)(); }
+};
+
+
+
+
+template <class T, class R>
+struct IsConst{
+};
+template <class T, class R>
+struct IsConst<const T, R>{
+	typedef R type;
+};
+template <class T, class R>
+struct IsNonConst{
+	typedef R type;
+};
+template <class T, class R>
+struct IsNonConst<const T, R>{
+};
+
+
+
+
+template <class M>
+typename IsConst<M,
+TransposeView<M,true> >::type Transpose(M &parent){
+	return TransposeView<M,true>(parent);
+}
+template <class M>
+typename IsNonConst<M,
+TransposeView<M,false> >::type Transpose(M &parent){
+	return TransposeView<M,false>(parent);
+}
+
+template <class SRC, class DST>
+void Copy(const SRC &src, const DST &dst_){
+	DST &dst = const_cast<DST&>(dst_);
+	dst() = src();
+}
+
+template <bool SrcRO>
+void Copy(const TransposeView<typename TerneryTruth<SrcRO,const TMatrix<int>, TMatrix<int> >::type,SrcRO> &src, TMatrix<int> &dst){
+	std::cout << "In specialization const" << std::endl;
+	dst() = src();
+}
+
+
+int main(){
+	const TMatrix<int> A;
+	TMatrix<int> B;
+	
+	B() = 1;
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	Copy(A, Transpose(B));
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	
+	
+	B() = 1;
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	Copy(Transpose(A), B);
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	
+	
+	B() = 1;
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	Copy(A, B);
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	
+	
+	B() = 1;
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	Copy(Transpose(A), Transpose(B));
+	std::cout << "A: " << A() << ", B: " << B() << std::endl;
+	
+	
+	TMatrix<int> C;
+	TMatrix<int> D;
+	
+	D() = 1;
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	Copy(C, Transpose(D));
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	
+	
+	D() = 1;
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	Copy(Transpose(C), D);
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	
+	
+	D() = 1;
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	Copy(C, D);
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	
+	
+	D() = 1;
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	Copy(Transpose(C), Transpose(D));
+	std::cout << "C: " << C() << ", D: " << D() << std::endl;
+	return 0;
+}
+
+
+
